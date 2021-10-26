ff62e09bd2595ef2373dc051689e685dd80ed335
gibsonjnr <ubonggibson7@gmail.com>
2021-10-26 23:38:08 +0100

    My first commit

---
+++ index.py
@@ -0,0 +1,69 @@
+Fname = 'Print' #first python code!!!
+print(Fname)
+
+firstStr = 'string'
+print(firstStr)
+
+
+num: int = 2
+
+num: float = 2.5
+
+array = ['Shine' , 'Dan' , 'Ben']
+
+tuple = ('Shine' , 'Dan' , 'Ben')
+
+boolean = True & False
+
+setList = {'Shine','Dan' , 'Dan' , 'Ben'}
+print(setList) #It keeps data unique!
+
+
+print(str(setList))
+print(int(num))
+
+
+Add = 5
+print(Add * 7)
+
+
+div = 8 // 3
+print('output =' , div)
+var = 6
+var += 2
+print(var)
+
+
+
+
+Fname = 'Ubong'
+Lname = 'Gibson'
+print(Fname + " " + Lname)
+
+
+num1 = [9]
+num2  = [9]
+print (num1 != num2)
+
+print(not(False))
+print(None or False or 'Hi' or True)
+print(None and '')
+
+print('' if False else 'Hello!!')
+num = 21
+evenORodd = "even" if num % 2 == 0 else 'odd'
+print(evenORodd)
+
+#IN Operator
+
+print( 'Dan' in array) #Used an already created array from line--12!!
+
+
+#IS Operator
+
+# checks if two varible have the same ID
+
+print(id(num1))
+print(id(num2))
+
+print(num1 is num2)
